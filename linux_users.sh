434d91c8e4493a026c0cd126780c874a79e26cf9
msylla01 <mohamed.sylla@aims-senegal.org>
2022-08-08 16:10:57 +0000

    usernam

--- linux_users.sh
+++ linux_users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username please : " username
+	read -p "Enter username please please : " username
 	read -s -p "Enter very strong password please : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
